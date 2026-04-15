e244865282a8e008e1482bda249f5eb87f5ba5c9
Sharif Uddin <suddin@connexpay.com>
2026-04-15 14:21:32 +0600

    00:39

---
+++ src/components/JSXRules.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const JSXRules = () => {
+  return (
+    <div>
+      <h1>JSXRules</h1>
+      <p> Rules</p>
+        <ul>
+          <li>JSX must return a singple parent element</li>
+          <li>JSX element must be properly closed</li>
+          <li>JSX attributes are written using caselCase(e.g, className instead of class).</li>
+        </ul>
+      
+    </div>
+  )
+}
+
+export default JSXRules
