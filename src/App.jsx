64e1ec5647289817d71a0321b8a6fbb698a1c85f
Sharif Uddin <suddin@connexpay.com>
2026-04-15 17:58:21 +0600

    default-main

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,9 @@
-import ProductInfo from "./components/ProductInfo";
-import Person from "./components/Person";
-
 const App = () => {
-  const name = "Mohammad";
-  const age = 26;
-  const products = [
-      {
-        id: 1,
-        name: "Phone",
-        price: "$669"
-      },
-      {
-        id: 2,
-        name: "Phone",
-        price: "$669"
-      },
-      {
-        id: 3,
-        name: "Phone",
-        price: "$669"
-      },
-      {
-        id: 4,
-        name: "Phone",
-        price: "$669"
-      },
-    ];
+  
 
   return (
     <div>
-      <Person 
-        name={name}
-        age={age}
-      />
-      <ProductInfo
-        products={products}
-      />
+      
     </div>
   );
 }
