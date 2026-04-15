e04d195a9fc348d13cdad8fc8ce81956a48c2cc6
Sharif Uddin <suddin@connexpay.com>
2026-04-15 13:35:07 +0600

    first-commit

---
+++ src/components/Add.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Geeting = () => {
+  return (
+    <div>Geeting</div>
+  )
+}
+
+export default Geeting
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainContent = () => {
+  return (
+    <main>
+      <h2>Main content</h2>
+      <p>This is the main content.</p>
+    </main>
+  )
+}
+
+export default MainContent
