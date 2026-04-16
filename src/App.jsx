e06a05e92babee80cbfd16109d23010df2528f2f
Sharif Uddin <suddin@connexpay.com>
2026-04-16 15:03:28 +0600

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import StyledCard from './components/StyledCard';
-import ProfileCard from './components/ProfileCard';
-import IconComponent from './components/IconComponent';
+import React, { useState } from 'react'
 
 const App = () => {
-  
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
   return (
     <div>
-      <StyledCard/>
-      <ProfileCard/>
-      <IconComponent/>
+      <h1>{count}</h1>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
     </div>
+    
   );
 }
 
