e615be7109dfdedf3aeecb9f4caf47e089c2e8b3
Sharif Uddin <suddin@connexpay.com>
2026-04-16 15:57:23 +0600

    02:05:00

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,39 @@
 import React, { use, useState } from 'react'
 
 const App = () => {
-  const [movie, setMovie] = useState(
+  const [movies, setMovies] = useState([
     {
+      id: 1,
       title: 'ABC',
-      ratting: 7.8,
-    });
+      rating: 7.8,
+    },
+    {
+      id: 2,
+      title: 'ABC',
+      rating: 7.8,
+    },
+    {
+      id: 3,
+      title: 'ABC',
+      rating: 7.8,
+    }]);
   
   const handleClick = () => {
-    // const copyMovie = {
-    //   ...movie,
-    //   ratting: 5
-    // }
-
-    setMovie({...movie, ratting: 5});
+    setMovies(
+      movies.map((m) => (m.id === 2 ? {...m, title: "EFG"} : m))
+    );
   }
 
   return (
-    <div>
-      <h1>Title : {movie.title}</h1>
-      <h2>Ratting : {movie.ratting}</h2>
+    <section>
+      {
+        movies.map(m => (
+          <li key={Math.random()}>Id: {m.id}, title: {m.title}</li>
+        ))
+      }
 
-      <button onClick={handleClick}>Change Movie Rattings</button>
-    </div>
+      <button onClick={handleClick}>Change Movie Name of 2</button>
+    </section>
     
   );
 }
