1ac2fb872057b9e24d6ff37ff87b7eeefb604e84
Habibur Rahman <habibur0199mhr@gmail.com>
2026-04-17 20:42:19 +0600

    default main  ok now

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-import Footer from "./components/Footer";
-import Greet from "./components/Greet";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-
 const App = () => {
   return (
     <div>
-      <Header/>
-      <MainContent/>
-      <Footer/>
+      
     </div>
   );
 }
--- src/components/Add.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Geeting = () => {
-  return (
-    <div>Geeting</div>
-  )
-}
-
-export default Geeting
