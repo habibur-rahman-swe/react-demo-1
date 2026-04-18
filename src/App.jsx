60fb3446e98e11e31ed269ae7ef7c8cda4941988
Habibur Rahman <habibur0199mhr@gmail.com>
2026-04-18 21:19:47 +0600

    use-state in a object

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import Counter from "./components/Counter";
+import Profile from "./components/Profile";
+import ShoppingList from "./components/ShoppingList";
 import TodoList from "./components/TodoList";
 
 const App = () => {
@@ -6,6 +8,8 @@ const App = () => {
     <div>
       <Counter/>
       <TodoList></TodoList>
+      <Profile></Profile>
+      <ShoppingList></ShoppingList>
     </div>
   );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,11 +4,13 @@ const Counter = () => {
 
     const [count, setCount] = useState(0);
     const increment = () => setCount(count + 1);
+    const decrement = () => setCount(count - 1);
 
     return (
         <div>
             <h1>{count}</h1>
             <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
         </div>
     )
 }
